4f2dad33ea7ae8b3fe4dbd930b1187021536df8d
225junior <225juniorbrindou@gmail.com>
2020-06-10 00:19:21 +0000

    url pattern

--- BigSlam/urls.py
+++ BigSlam/urls.py
@@ -27,7 +27,3 @@ urlpatterns = [
     path('game/', include('game.urls')),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
